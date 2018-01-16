1a70e435d5c4862b35aad3c2eacdd08b9f5e5c28
Mikael M <mikael_96@live.com>
2018-01-16 11:21:32 +0200

    updated my app

--- Documents/Tasks/WBMA_course/w1t1/src/app/app.module.ts
+++ Documents/Tasks/WBMA_course/w1t1/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    MainContentComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
